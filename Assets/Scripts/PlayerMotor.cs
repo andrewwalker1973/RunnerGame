a2ce9aff5d8edde4882946d3fa37b39e98e5c4e8
Andrew Walker <andrew.walker73@gmail.com>
2020-09-09 22:43:59 +0200

    mobileInput done

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -7,33 +7,48 @@ public class PlayerMotor : MonoBehaviour
 
     private const float LANE_DISTANCE = 2.5f; //set the lane width
     private const float TURN_SPEED = 0.05f;
+
+
+    //  Animation
+    private Animator anim;
     // Movement
     private CharacterController controller;
-    private float jumpForce = 100.0f; // WAS 4
-    private float gravity = 500.0f; // WAS 12
+    private float jumpForce = 4.0f; // WAS 4
+    private float gravity = 12f; // WAS 12
     private float verticalVelocity;
     private float speed = 7.0f;
+   
     private int desiredLane = 1; // 0=left 1=middle 2=right
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
         // gather the inputs on which lane we should be in
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        { 
-            // move left
+        if (MobileInput.Instance.SwipeLeft)
+        {
             MoveLane(false);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (MobileInput.Instance.SwipeRight)
         {
-            // move right
             MoveLane(true);
         }
 
+        /*  if (Input.GetKeyDown(KeyCode.LeftArrow))
+          { 
+              // move left
+              MoveLane(false);
+          }
+          if (Input.GetKeyDown(KeyCode.RightArrow))
+          {
+              // move right
+              MoveLane(true);
+          }
+  */
         // Calculate where we should be in the future
         Vector3 targetPosition = transform.position.z * Vector3.forward;
         if (desiredLane == 0)
@@ -48,40 +63,45 @@ public class PlayerMotor : MonoBehaviour
 
         // Calcuate move vector
         Vector3 moveVector = Vector3.zero;
-        // moveVector.x = (targetPosition - transform.position).normalized.x * speed; // character was shakaing 
+       //  moveVector.x = (targetPosition - transform.position).normalized.x * speed; // character was shakaing 
 
         moveVector.x = (targetPosition - transform.position).x * speed;
 
-        // Calc Y
-        if (IsGrounded()) //if grounded
-        {
+        
 
-            Debug.Log("IsGrounded ");
+        // Calc Y
+        if (controller.isGrounded) //if grounded
+        {
+            //bool isGrounded = true;
+            anim.SetBool("Grounded",true);
 
             verticalVelocity = -0.1f;
-            Debug.Log("verticalVelocity 1" + verticalVelocity);
+           
 
-            if (Input.GetKeyDown(KeyCode.Space))
+         //   if (Input.GetKeyDown(KeyCode.Space))
+         if (MobileInput.Instance.SwipeUp)
             {
                 //Jump
+                anim.SetTrigger("Jump");
                 verticalVelocity = jumpForce;
-                Debug.Log("Jump Pressed");
-                //  Debug.Log("verticalVelocity " + verticalVelocity);
+              
 
-            }
-            else
-            {
-
-                Debug.Log("NOTGrounded "); verticalVelocity -= (gravity * Time.deltaTime); // slowly fall to ground level
-                Debug.Log("Floating");
-                // Fast Falling area
-                if (Input.GetKeyDown(KeyCode.M))
-                {
-                    Debug.Log("m Pressed");
-                    verticalVelocity = -jumpForce;  //drop immediatly to ground
-                }
             }
         }
+        else
+        {
+           // bool isGrounded = false;
+            //  Debug.Log("NOTGrounded "); 
+            verticalVelocity -= (gravity * Time.deltaTime); // slowly fall to ground level
+          //  Debug.Log("Floating");
+            // Fast Falling area
+           // if (Input.GetKeyDown(KeyCode.M))
+           if (MobileInput.Instance.SwipeDown)
+            {
+                verticalVelocity = -jumpForce;  //drop immediatly to ground
+            }
+        }
+        
         
         moveVector.y = verticalVelocity;
         moveVector.z = speed;
@@ -94,9 +114,10 @@ public class PlayerMotor : MonoBehaviour
         //Rotate charatcter in direction of travel
         Vector3 dir = controller.velocity;
         if (dir != Vector3.zero)
-        {
+       {
             dir.y = 0;
-            transform.forward = Vector3.Lerp(transform.forward, dir, TURN_SPEED);
+             transform.forward = Vector3.Lerp(transform.forward, dir, TURN_SPEED);
+            
         }
         
     }
@@ -108,14 +129,15 @@ public class PlayerMotor : MonoBehaviour
 
     }
 
-    private bool IsGrounded()
-    {
-        Ray groundRay = new Ray(new Vector3(controller.bounds.center.x, (controller.bounds.center.y - controller.bounds.extents.y) + 0.2f, controller.bounds.center.z),Vector3.down);
-        Debug.DrawRay(groundRay.origin, groundRay.direction, Color.cyan, 1.0f);
+  //  private bool IsGrounded()
+  //  {
+        //Ray groundRay = new Ray(new Vector3(controller.bounds.center.x, (controller.bounds.center.y - controller.bounds.extents.y) + 0.2f, controller.bounds.center.z),Vector3.down);
+  //      Ray groundRay = new Ray(new Vector3(controller.bounds.center.x, (controller.bounds.center.y - controller.bounds.extents.y), controller.bounds.center.z), Vector3.down);
+  //      Debug.DrawRay(groundRay.origin, groundRay.direction, Color.cyan, 1.0f);
 
+
+   //      return Physics.Raycast(groundRay, 0.001f + 0.1f);
         
-        return Physics.Raycast(groundRay, 0.5f + 0.1f);
-        
-    }
+ //   }
     
 }
