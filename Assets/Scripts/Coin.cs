20f98695652f1ef8161c4ac2b8d6391e7ff0a9e0
Andrew Walker <andrew.walker73@gmail.com>
2020-11-09 22:30:14 +0200

    added sound manager

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -11,12 +11,13 @@ public class Coin : MonoBehaviour
     // private Animator anim;
     //   CoinMove coinMoveScript;
 
-    public AudioClip CollectCoinsSFX;
-    private SoundManager _soundManager;
+    ////public AudioClip CollectCoinsSFX;
+   // private SoundManager _soundManager;
     private GameObject _player;
     private readonly int _speed = 30;
-   // private AudioSource source;
-
+    // private AudioSource source;
+    private AudioManager audioManager;
+    public string coinSoundName;
     // try fix coinnot leaving
     // private GameObject objectToDeactivate;
 
@@ -25,14 +26,18 @@ public class Coin : MonoBehaviour
     {
         //    anim = GetComponent<Animator>();
         //  objectToDeactivate.SetActive(true);
-        _soundManager = GetComponent<SoundManager>();
+     //   _soundManager = GetComponent<SoundManager>();
     }
 
     private void Start()
     {
         //     playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         //     coinMoveScript = gameObject.GetComponent<CoinMove>();
-        
+        audioManager = AudioManager.instance;
+        if (audioManager == null)
+        {
+            Debug.LogError("No audio manager found in scene");
+        }
 
     }
 
@@ -41,7 +46,7 @@ public class Coin : MonoBehaviour
     {
         if (_player != null)
         {
-            print("Coin moving");
+          //  print("Coin moving");
             float step = _speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, step);
         }
@@ -80,7 +85,9 @@ public class Coin : MonoBehaviour
     public void Collect()
     {
         //Debug.Log("Coin Collected");
-        _soundManager.PlaySFXClip(CollectCoinsSFX);
+        //  _soundManager.PlaySFXClip(CollectCoinsSFX);
+        audioManager.PlaySound(coinSoundName);
+
         StartCoroutine(RemoveGameObject());
         
     }
